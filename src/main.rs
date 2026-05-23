e8a40589fe36dd8e50488007d1a786a550224d52
K <mango1472@outlook.kr>
2026-05-23 12:37:42 +0900

    Add Release

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use o_::{AppError, args::Args, conf, report};
 use std::{fs, process};
 
 fn main() {
-    if let Err(error) = try_main() {
+    if let Err(error) = _main() {
         report::print_error(&error.report());
         process::exit(1);
     }
 }
 
-fn try_main() -> Result<(), AppError> {
+fn _main() -> Result<(), AppError> {
     let home = home::home_dir().ok_or(AppError::HomeDirUnavailable)?;
     let mut config = home;
     config.push(".config");
